9d9f4c3cd0ddbc2a2b1cdab8f1a82baeb7bb3937
mdasifalijihat <mdasifalijihat@gmail.com>
2025-11-25 13:45:19 +0600

    added get method

---
+++ src/config/index.ts
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.join(process.cwd(), ".env") });
+
+const config = {
+  env: process.env.MODE_ENV ? Number(process.env.MODE_ENV) : 5000,
+  port: process.env.PORT,
+};
+
+export default config;
--- src/server.ts
+++ src/server.ts
@@ -1,58 +1,56 @@
 import http, { IncomingMessage, Server, ServerResponse } from "http";
-import config from "./intex";
+import config from "./config";
+import { RouteHandler, routes } from "./helpers/RouteHandler";
+import "./routes";
+
 const server: Server = http.createServer(
   (req: IncomingMessage, res: ServerResponse) => {
     console.log("server is running.......");
+    const method = req.method?.toUpperCase() || "";
+    const path = req.url || "";
 
-    // root route
-    if (req.url == "/" && req.method == "GET") {
-      res.writeHead(200, { "content-type": "application/json" });
+    const methodMap = routes.get(method);
+    const handler: RouteHandler | undefined = methodMap?.get(path);
+
+    if (handler) {
+      handler(req, res);
+    } else {
+      res.writeHead(404, { "content-type": "application/json" });
       res.end(
         JSON.stringify({
-          message: "hello from node js with typescript",
-          path: req.url,
-        })
-      );
-    }
-
-    // status route
-    if (req.url === "/api" && req.method == "GET") {
-      res.writeHead(200, { "content-type": "application/json" });
-      res.end(
-        JSON.stringify({
-          message: "health status okay ",
-          path: req.url,
+          success: false,
+          message: "Route not found!!!",
+          path,
         })
       );
     }
 
     // post route
+    //     if (req.url === "/api/users" && req.method == "POST") {
+    //       // const user = {
+    //       //         id: 1,
+    //       //         name: "Asif",
+    //       //         email: "asifali@gmail.com"
+    //       // }
+    //       // res.writeHead(200, {"content-type" : "application/json"});
+    //       // res.end(JSON.stringify(user))
 
-    if (req.url === "/api/users" && req.method == "POST") {
-      // const user = {
-      //         id: 1,
-      //         name: "Asif",
-      //         email: "asifali@gmail.com"
-      // }
-      // res.writeHead(200, {"content-type" : "application/json"});
-      // res.end(JSON.stringify(user))
+    //       let body = "";
 
-      let body = "";
+    //       req.on("data", (chunk) => {
+    //         body += chunk.toString();
+    //       });
 
-      req.on("data", (chunk) => {
-        body += chunk.toString();
-      });
-
-      req.on("end", () => {
-        try {
-          const parseBody = JSON.parse(body);
-          console.log(parseBody);
-          res.end(JSON.stringify(parseBody));
-        } catch (err: any) {
-          console.log(err?.message);
-        }
-      });
-    }
+    //       req.on("end", () => {
+    //         try {
+    //           const parseBody = JSON.parse(body);
+    //           console.log(parseBody);
+    //           res.end(JSON.stringify(parseBody));
+    //         } catch (err: any) {
+    //           console.log(err?.message);
+    //         }
+    //       });
+    //     }
   }
 );
 
