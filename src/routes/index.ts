898f59b7bbafe27e170c0acaace44078a888af4f
mdasifalijihat <mdasifalijihat@gmail.com>
2025-11-25 15:03:00 +0600

    added put method

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,3 +1,4 @@
+import { readUsers, writeUsers } from "../helpers/fileDb";
 import parseBody from "../helpers/parseBody";
 import addRoutes from "../helpers/RouteHandler";
 import sendJson from "../helpers/sendJson";
@@ -18,5 +19,43 @@ addRoutes("GET", "/api", (req, res) => {
 
 addRoutes("POST", "/api/users", async (req, res) => {
   const body = await parseBody(req);
-  sendJson(res, 201, body);
+
+  // user json read
+  const users = readUsers();
+
+  const newUser = {
+    id: Date.now(),
+    ...body,
+  };
+
+  users?.push(newUser);
+
+  writeUsers(users);
+
+  sendJson(res, 201, { success: true, data: body });
+});
+
+addRoutes("PUT", "/api/users/:id", async (req, res) => {
+  const { id } = (req as any).params;
+  const body = await parseBody(req);
+  const users = readUsers();
+  const index = users.findIndex((user: any) => user.id == id);
+
+  if (index === -1) {
+    sendJson(res, 404, {
+      success: false,
+      message: "User nor found this sever",
+    });
+  }
+  users[index] = {
+    ...users[index],
+    ...body,
+  };
+
+  writeUsers(users);
+  sendJson(res, 202, {
+    success: true,
+    message: ` id ${id} user updated `,
+    data: users[index],
+  });
 });
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import http, { IncomingMessage, Server, ServerResponse } from "http";
 import config from "./config";
 import { RouteHandler, routes } from "./helpers/RouteHandler";
 import "./routes";
+import findDynamicRoute from "./helpers/dynamicRouteHandler";
 
 const server: Server = http.createServer(
   (req: IncomingMessage, res: ServerResponse) => {
@@ -14,6 +15,10 @@ const server: Server = http.createServer(
 
     if (handler) {
       handler(req, res);
+    } else if (findDynamicRoute(method, path)) {
+      const match = findDynamicRoute(method, path);
+      (req as any).params = match?.params;
+      match?.handler(req, res);
     } else {
       res.writeHead(404, { "content-type": "application/json" });
       res.end(
